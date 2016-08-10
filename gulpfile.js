3905ef40e9bb7fa13b2fc8fdb5f2820f79e0f3d3
jasconchen <jasonchenzehui@gmail.com>
2016-08-10 14:31:52 +0800

    发布打包

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,34 @@
 var gulp = require('gulp');
+var rev = require('gulp-rev');
+var revReplace = require('gulp-rev-replace');
+var useref = require('gulp-useref');
+var filter = require('gulp-filter');
+var uglify = require('gulp-uglify');
+var csso = require('gulp-csso');
+var imagemin = require('gulp-imagemin');
 
-gulp.task('hello', function(){
-	console.log('Hello Gulp!');
+
+gulp.task('default', function(){
+    var jsFilter = filter('**/*.js', {restore: true});
+    var cssFilter = filter('**/*.css', {restore: true});
+    var indexHtmlFilter = filter(['**/*', '!**/index.html'], {restore: true});
+
+    return gulp.src('src/index.html')
+        .pipe(useref())
+        .pipe(jsFilter)
+        .pipe(uglify())
+        .pipe(jsFilter.restore)
+        .pipe(cssFilter)
+        .pipe(csso())
+        .pipe(cssFilter.restore)
+        .pipe(indexHtmlFilter)
+        .pipe(rev())
+        .pipe(indexHtmlFilter.restore)
+        .pipe(revReplace())
+        .pipe(gulp.dest('dist'));
+})
+.task('default', function(){
+    return gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
 });
